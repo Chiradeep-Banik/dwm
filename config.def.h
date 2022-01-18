bcc854e019e602845cbe798d9581e271626e340e
Chiradeep-Banik <chiradeep13@gmail.com>
2022-01-18 17:33:22 +0530

    Changed the color

--- config.def.h
+++ config.def.h
@@ -12,7 +12,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#2d9191";
+static const char col_cyan[]        = "#2b3a6e";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
